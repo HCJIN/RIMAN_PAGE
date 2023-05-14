c0dfd16d6a56412298c163a943c311906a39e969
HCJIN <ghkdckswls02@naver.com>
2023-05-15 00:08:09 +0900

    first commit

--- src/component/Main_Visual.js
+++ src/component/Main_Visual.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import BG_1 from '../image/BG_1.jpg'
-import BG_2 from '../image/BG_2.jpg'
-import BG_3 from '../image/BG_3.jpg'
+import React, { useState, useEffect } from 'react';
+import BG_1 from '../image/BG_1.jpg';
+import BG_2 from '../image/BG_2.jpg';
+import BG_3 from '../image/BG_3.jpg';
 
 const Main_Visual = () => {
+  const [bgIndex, setBgIndex] = useState(0);
+  const bgList = [BG_1, BG_2, BG_3];
+
+  useEffect(() => {
+    const bgTimer = setInterval(() => {
+      setBgIndex(bgIndex => (bgIndex + 1) % bgList.length);
+    }, 5000);
+    return () => clearInterval(bgTimer);
+  }, []);
+
   return (
-    <div>
-        <div className='BG_Image'>
-            <img src={BG_1} className='BG_1' alt='BG_1'></img>
-            <img src={BG_2} className='BG_2' alt='BG_2'></img>
-            <img src={BG_3} className='BG_3' alt='BG_3'></img>
-        </div>
-
+    <div className="BG_Image">
+      {bgList.map((bg, index) => (
+        <img
+          key={index}
+          src={bg}
+          className={`BG_Image__${index} ${
+            index === bgIndex ? 'active' : ''
+          }`}
+          alt={`BG_${index}`}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Main_Visual
+export default Main_Visual;
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,71 +4,89 @@ import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { useState } from 'react';
 
 const NavBar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  
+  const handleMouseEnter = () => {
+    setShowDropdown(true);
+  };
+  
+  const handleMouseLeave = () => {
+    setShowDropdown(false);
+  };
+  
   return (
     <div className='main'>
       <div className='header'>
-        <img src='https://www.rimankorea.com/logo_rimankorea.svg?30b253deffbf5c06549e270cc61b2f69' alt='로고' className='logo'/>
-        <ul className='menu-list'>
-          <li className='menu_1' >
-            <a href='#' >BRANDS</a>
-            <div className='item_contents'>
-              <div className='contents_menu'>
-                <ul className='contents_menu_list'>
-                    <li>인셀덤</li>
-                    <li>보타랩</li>
-                    <li>라이프닝</li>
-                </ul>
-                <ul className='contents_menu_list'>
-                    <li>제품보기</li>
-                </ul>
-                <ul className='contents_menu_list'>
-                    <li>리만뉴스</li>
-                    <li>소셜미디어</li>
-                    <li>브랜드필름</li>
-                    <li>뷰티 & 라이프 LAB</li>
-                </ul>
-                <ul className='contents_menu_list'>
-                    <li>기업소개</li>
-                    <li>CI/BI 소개</li>
-                    <li>R&D</li>
-                    <li>ESG경영</li>
-                    <li>연혁 & 수상</li>
-                    <li>VISIT OUR BRANCH</li>
-                    <li>CONTACT US</li>
-                </ul>
-                <ul className='contents_menu_list'>
-                    <li>직무소개</li>
-                    <li>인사제도</li>
-                    <li>복지제도</li>
-                    <li>채용공고</li>
-                </ul>
-              </div>
-            </div>
-          </li>
-          <li className='menu_1'>
-            <a href=''>PRODUCTS</a>
-          </li>
-          <li className='menu_1'>
-            <a href=''>NEWS & MEDIA</a>
-          </li>
-          <li className='menu_1'>
-            <a href=''>ABOUT US</a>
-          </li>
-          <li className='menu_1'>
-            <a href=''>CAREERS</a>
-          </li>
-          <li className='menu_1'>
-            <a href=''>RIMAN MALL</a>
-          </li>
-        </ul>
-        <div className='Login'>
+        <a>
+          <img src='https://www.rimankorea.com/logo_rimankorea.svg?30b253deffbf5c06549e270cc61b2f69' alt='로고'/>
+        </a>
+        <ul className='menu_list'>
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='main_menu'>
+          <a href='#'>BRAND</a>
+          {showDropdown && (
+            <ul className='sub_menu'>
+              <li>인셀덤</li>
+              <li>보타랩</li>
+              <li>라이프닝</li>
+            </ul>
+          )}
+        </li>
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='main_menu'>
+          <a href='#'>PRODUCTS</a>
+          {showDropdown && (
+            <ul className='sub_menu'>
+              <li>제품보기</li>
+            </ul>
+          )}
+        </li>
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='main_menu'>
+          <a href='#'>NEWS & MEDIA</a>
+          {showDropdown && (
+            <ul className='sub_menu'>
+              <li>리만뉴스</li>
+              <li>소셜미디어</li>
+              <li>브랜드 필름</li>
+              <li>뷰티 & 라이프 LAB</li>
+            </ul>
+          )}
+        </li>
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='main_menu'>
+          <a href='#'>ABOUT US</a>
+          {showDropdown && (
+            <ul className='sub_menu'>
+              <li>기업소개</li>
+              <li>CI/BI 소개</li>
+              <li>R & D</li>
+              <li>ESG 경영</li>
+              <li>연혁 & 수상</li>
+              <li>VISIT OUR BRANCH</li>
+              <li>CONTACT US</li>
+            </ul>
+          )}
+        </li>
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='main_menu'>
+          <a href='#'>CAREERS</a>
+          {showDropdown && (
+            <ul className='sub_menu'>
+              <li>직무소개</li>
+              <li>인사제도</li>
+              <li>복지제도</li>
+              <li>채용공고</li>
+            </ul>
+          )}
+        </li>
+        <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='main_menu'>
+          <a href='#'>RIMAN MALL</a>
+        </li>
+      </ul>
+      <div className='Login'>
           <FontAwesomeIcon icon={faUser} className='My_page_Icon'/>
           <div className='My_Page'>My Office</div>
           <div className='Menu_Bar'>=</div>
         </div>
       </div>
+      
     </div>
-  )
-}
-
+  );
+};
 export default NavBar
