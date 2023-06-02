6d0a436a7fedf568a684692abf3287c1c0f85a79
HCJIN <ghkdckswls02@naver.com>
2023-06-02 13:19:20 +0900

    News_Page_end

--- src/component/News_Page.js
+++ src/component/News_Page.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import News_Img_1 from '../image/892e46a2-1e39-4f16-8094-8b70be1def68.jpg'
+import News_Img_2 from '../image/c5e718c0-31e2-4237-8c98-03a2e1ac05a9.jpg'
+import News_Img_3 from '../image/4c758f54-b0cd-4f2c-b3d3-ee5fdcbd3fc1.jpg'
 
 const News_Page = () => {
   return (
@@ -12,13 +15,31 @@ const News_Page = () => {
         </div>
         <div className='Main_News_Content'>
           <div className="Content">
-            <img src=""/>
+            <img src={News_Img_1}/>
+            <div className='Main_News_Title'>
+              리만코리아, '서울 사랑의열매'에 3억 원 상당 보타랩 제품 기부
+            </div>
+            <div className='Main_News_Date'>
+              2023.05.24
+            </div>
           </div>
           <div className="Content">
-            <img src=""/>
+            <img src={News_Img_2}/>
+            <div className='Main_News_Title'>
+              {`제주산 최고 품질 원료 고집하는 <리만코리아>`} 
+            </div>
+            <div className='Main_News_Date'>
+              2023.05.17
+            </div>
           </div>
           <div className="Content">
-            <img src=""/>
+            <img src={News_Img_3}/>
+            <div className='Main_News_Title'>
+              오늘과 내일의 건강한 활력을 책임지는 건강기능식품 제안 라이프닝 액티브 에너지 샷 & 라이프닝 리셋...
+            </div>
+            <div className='Main_News_Date'>
+              2023.05.24
+            </div>
           </div>
         </div>
       </div>
