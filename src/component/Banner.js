618b9194022514a4a7af7d02d5a5b3ae9980ca74
HCJIN <ghkdckswls02@naver.com>
2023-09-18 13:19:08 +0900

    CSS_Change

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
       <img src={Top_Banner_Img}/>
       <div className='Top_Banner_Txt'>
         <h1 className='Top_Banner_Box_Top_Txt'>
-          청정 제주 화산 암반층이 품어온 미네랄
+          제주 화산 암반층이 품어온 미네랄
         </h1>
         <div className='Top_Banner_Box_Mid_Txt'>
           용암해수는 제주바다가 만들어 낸 맑고 깨끗한 물입니다.
@@ -26,7 +26,7 @@ const Banner = () => {
         </h1>
         <div className='Bottom_Banner_Box_Mid_Txt'>
           병풀은 호랑이가 상처를 입었을 때 치료하는 모습을 보고 호랑이풀이라 불렀고
-          <br/> 코끼리가 병풀을 먹는 것을 보고 장수식품으로 여겼다고 합니다.
+          <br/> 코끼리가 먹는 것을 보고 장수식품으로 여겼다고 합니다.
         </div>
         <button className="Bottom_Banner_Box_Btn">자세히 보기</button>     
       </div>
